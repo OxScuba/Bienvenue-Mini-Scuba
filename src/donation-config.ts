bf61304baf2786d43d812daf5e0be7f929bce344
OxScuba <fernandez.marco@outlook.fr>
2026-07-30 14:37:29 +0200

    update  birth tx

--- src/donation-config.ts
+++ src/donation-config.ts
@@ -5,8 +5,8 @@ export const donationConfig = {
   lightningQr: "./assets/qr/lightning.webp",
 
   // À compléter après la naissance. Laisser vide jusque-là.
-  birthBlockHeight: "",
-  birthTransactionId: "",
-  birthTransactionUrl: "",
-  birthOpReturnMessage: "",
+  birthBlockHeight: "960 248",
+  birthTransactionId: "63f583181fbe9b6a7959aa400a1ca81f3fd6a5d031a1d1254358113c2bd72c47",
+  birthTransactionUrl: "https://mempool.space/tx/63f583181fbe9b6a7959aa400a1ca81f3fd6a5d031a1d1254358113c2bd72c47",
+  birthOpReturnMessage: "Bienvenue au monde Enora. Aimée pour toujours. Ta liberté, ton horizon. M&V",
 };
